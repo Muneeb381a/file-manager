7c0c3c162c27fe520c7b6dc648353444dd5ec7f6
Muneeb381a <muneebafzal381a@gmail.com>
2025-10-30 22:30:07 +0500

    Update CORS origins and fix Vercel configuration for deployment

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost:5173",  # React local dev
-    "http://127.0.0.1:5173",  # Sometimes used by Vite
+    "https://file-manager-frontend-virid.vercel.app",
 ]
 
 app.add_middleware(
